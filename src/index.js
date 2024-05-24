2929a0f03c408a104b6793f3eb7fc79ef3e044d1
Firkax <157710697+Firkax69@users.noreply.github.com>
2024-05-24 12:16:15 +0100

    feat: set data and DOM in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('yo KIV');
+const searchForm = document.getElementById('searchForm');
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+
+searchForm.addEventListener('submit', e => {
+    e.preventDefault()
+})
+
+searchBtn.addEventListener('click', async () => {
+    if (!searchInput.value) return;
+    // get data from weather();            *** NEEDS FINISHING ***
+    // set obtained data to view();        *** NEEDS FINISHING ***
+})
